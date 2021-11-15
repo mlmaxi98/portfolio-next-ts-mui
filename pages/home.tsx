c07155308db7c6ad0560c659b88e6f5528b50d10
mlmaxi98 <cardozojoaquinm@gmail.com>
2021-11-14 21:06:38 -0300

    ajustando variables

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography, Grid } from '@mui/material/';
+import { Box, Container, Grid } from '@mui/material/';
 import Title from '../components/Title';
 
 const Home = () => {
